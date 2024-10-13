ac377aa6dbf6ab95aeaf78db31ae8bf39d8f8731
Md Azizul Hoq <77917370+azizulhoq953@users.noreply.github.com>
2024-10-13 11:47:49 +0600

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -20,7 +20,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL || 'https://storytelling-server-w2bm-omiyze2zz-azizuls-projects-69ab18f1.vercel.app'
 });
 
 axiosInstance.interceptors.request.use(config => {
